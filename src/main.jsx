43270fd3dae1be25f9abb4a10ff2987bced755be
LautaroLeall <lautaroleal4@gmail.com>
2026-07-24 13:19:24 -0300

    refactor(entry): simplificar punto de entrada main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BookingProvider from './context/BookingProvider';
-import AuthProvider from "./context/AuthProvider.jsx";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import AuthProvider from './context/AuthProvider.jsx';
 import App from './App.jsx';
 import './index.css';
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <BookingProvider>
-          <App />
-        </BookingProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GoogleOAuthProvider clientId={clientId}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
+    </React.StrictMode>
 );
