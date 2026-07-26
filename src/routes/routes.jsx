a0e2f9adc408b0bc7d515453f87fff22b6dc68df
LautaroLeall <lautaroleal4@gmail.com>
2026-07-26 11:52:19 -0300

    feat(routes): estructurar rutas protegidas y validar build de produccion

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,36 +1,51 @@
 // src/routes/routes.jsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import NotFound from "../components/NotFound/NotFound";
-import Container from "../container/Container";
-import Home from "../components/Home/Home";
-import Carousel from "../components/Cars/Carousel";
-import CarDetail from "../components/CarDetail/CarDetail";
-import MyBookings from "../components/MyBooks/MyBookings";
-import MyPayments from "../components/MyPayments/MyPayments";
-import Contact from "../components/Contact/Contact";
-import Login from "../components/Auth/Login";
-import Register from "../components/Auth/Register";
-import PrivateRoute from "./PrivateRoute";
+import { Routes, Route } from 'react-router-dom';
+import Container from '../container/Container';
+import Inicio from '../components/Inicio/Inicio';
+import CarGrid from '../components/Cars/CarGrid';
+import CarDetail from '../components/CarDetail/CarDetail';
+import Login from '../components/Auth/Login';
+import Register from '../components/Auth/Register';
+import MyBookings from '../components/MyBookings/MyBookings';
+import MyPayments from '../components/MyPayments/MyPayments';
+import Profile from '../components/Profile/Profile';
+import NotFound from '../components/NotFound/NotFound';
+import PrivateRoute from './PrivateRoute';
+import AdminRoute from './AdminRoute';
+import AdminLayout from '../components/Admin/AdminLayout';
+import AdminDashboard from '../components/Admin/AdminDashboard';
+import AdminBookings from '../components/Admin/AdminBookings';
+import AdminUsers from '../components/Admin/AdminUsers';
+import AdminChats from '../components/Admin/AdminChats';
+import AdminCars from '../components/Admin/AdminCars';
 
-const AppRoutes = ({ cars }) => (
+const AppRoutes = () => (
     <Routes>
+        {/* PUBLICAS */}
         <Route path="/" element={<Container />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/cars" element={<Carousel cars={cars} />} />
-        <Route path="/car/:id" element={<CarDetail />} />
-        <Route path="/myBookings" element={<MyBookings />} />
-
-        <Route path="/myPayments" element={
-            <PrivateRoute>
-                <MyPayments />
-            </PrivateRoute>
-        } />
-
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/autos" element={<CarGrid />} />
+        <Route path="/autos/:id" element={<CarDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
+
+        {/* PRIVADAS */}
+        <Route path="/mireservas" element={<PrivateRoute><MyBookings /></PrivateRoute>} />
+        <Route path="/mispagos" element={<PrivateRoute><MyPayments /></PrivateRoute>} />
+        <Route path="/perfil" element={<PrivateRoute><Profile /></PrivateRoute>} />
+
+        {/* ADMIN */}
+        <Route path="/admin" element={<AdminRoute><AdminLayout /></AdminRoute>}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="autos" element={<AdminCars />} />
+            <Route path="reservas" element={<AdminBookings />} />
+            <Route path="chats" element={<AdminChats />} />
+            <Route path="usuarios" element={<AdminUsers />} />
+            <Route path="*" element={<AdminDashboard />} />
+        </Route>
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
     </Routes>
 );
 
