d270a7f3a802346f37e4fabb1c9e1d4dac735a35
LautaroLeall <lautaroleal4@gmail.com>
2026-07-26 11:57:39 -0300

    chore: configurar redirects en public y manual chunks en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-icons')) {
+              return 'vendor-icons';
+            }
+            if (id.includes('date-fns')) {
+              return 'vendor-date-fns';
+            }
+            if (id.includes('react-router-dom') || id.includes('react-dom') || id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('sweetalert2')) {
+              return 'vendor-swal';
+            }
+            return 'vendor-core';
+          }
+        }
+      }
+    }
+  }
+});
